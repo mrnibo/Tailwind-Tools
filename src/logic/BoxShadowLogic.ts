fc45fc1c3fa56368f48bf7e00ccb5ff6cfdab6a5
MrNibo99 <153727578+MrNibo99@users.noreply.github.com>
2024-07-14 12:15:39 +0200

    ...

---
+++ src/logic/BoxShadowLogic.ts
@@ -0,0 +1,22 @@
+export const generateTailwindBoxShadow = (
+  hOffset: number,
+  vOffset: number,
+  blur: number,
+  spread: number,
+  color: string,
+  inset: boolean
+): string => {
+  const colorWithOpacity = color + (Math.round(0.57 * 255)).toString(16).padStart(2, '0'); // Assuming 57% opacity
+  return `shadow${inset ? '-inset' : ''}-[${hOffset}px_${vOffset}px_${blur}px_${spread}px_${colorWithOpacity}]`;
+};
+
+export const generateVanillaBoxShadow = (
+  hOffset: number,
+  vOffset: number,
+  blur: number,
+  spread: number,
+  color: string,
+  inset: boolean
+): string => {
+  return `${inset ? 'inset ' : ''}${hOffset}px ${vOffset}px ${blur}px ${spread}px ${color}`;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+import BoxShadow from "@/components/tools/BoxShadow";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <BoxShadow />
+      </section>
+    </DefaultLayout>
+  );
+}
